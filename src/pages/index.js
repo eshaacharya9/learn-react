d1f8a14c51d50b851f80c47d8ae31a7cfe0bfdfc
esha <acharya.e@northeastern.edu>
2024-02-16 11:13:09 -0500

    recipies fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './qcomps/shoppingCart'
+import Profile from './qcomps/recipes'
 export default function Home() {
   return (
     <div className={styles.main}>
--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -16,9 +16,18 @@ export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      const listItems = recipes.map(recipe =>
-    <li>{person}</li>
-  );
+      {recipes.map((recipe) =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {Array.from(recipe.ingredients).map((ingredient) =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
